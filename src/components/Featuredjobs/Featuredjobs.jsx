f92e39725e13fb63196b11015230aeced1941a3a
rakibulhasan044 <rhrakib044@gmail.com>
2024-03-23 18:55:24 +0600

    show all jobs btn dynamic

--- src/components/Featuredjobs/Featuredjobs.jsx
+++ src/components/Featuredjobs/Featuredjobs.jsx
@@ -4,6 +4,11 @@ import Job from "../Job/Job";
 const Featuredjobs = () => {
   const [jobs, setJobs] = useState([]);
 
+  //this is not the best way to load data
+
+  const [dataLength, setDataLength] = useState(4);
+
+
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
@@ -19,9 +24,13 @@ const Featuredjobs = () => {
         </p>
       </div>
       <div className="grid grid-cols-2 gap-24">
-        {jobs.map((job) => (
+        {jobs.slice(0, dataLength).map((job) => (
           <Job key={job.id} job={job}></Job>
         ))}
+      </div>
+      <div className={ dataLength === jobs.length ? "hidden" : "text-center p-5"}>
+        <button
+        onClick={() => setDataLength(jobs.length)} className="btn btn-primary text-white">Show All Jobs</button>
       </div>
     </div>
   );
